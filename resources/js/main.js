924cc4bfcf5a25d82aba1eb83f2a23436878ea68
= <dhwngjs01@naver.com>
2023-05-07 23:54:17 +0900

    swiper-slide height

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,47 +1,48 @@
 const handleSlideMove = (slideNumber) => {
   $("ul.navbar-nav li a").removeClass("active");
 
-  if($(".navbar-collapse").hasClass("show")){
+  if ($(".navbar-collapse").hasClass("show")) {
     $(".navbar-toggler").click();
   }
 
   swiper.slideTo(slideNumber);
-}
+};
 
 const navColorChange = (currentSlide) => {
-  if(currentSlide == 0) {
+  if (currentSlide == 0) {
     $("#nav").removeClass("navbar-scroll");
     $("ul.navbar-nav li a").removeClass("active");
   } else {
     $("#nav").addClass("navbar-scroll");
-    $("ul.navbar-nav li a").eq(currentSlide - 1).addClass("active");
+    $("ul.navbar-nav li a")
+      .eq(currentSlide - 1)
+      .addClass("active");
   }
-}
-
+};
 
 const swiper = new Swiper(".swiper-container", {
-  speed : 500,
-  direction : "vertical",
-  mousewheel : true,
-  slidesPerView : 1,
-  on : {
-    touchMove : function(){
-      if(this.realIndex == 0){
+  speed: 500,
+  direction: "vertical",
+  mousewheel: true,
+  slidesPerView: "auto",
+  on: {
+    touchMove: function () {
+      if (this.realIndex == 0) {
         this.allowSlidePrev = false;
       } else {
         this.allowSlidePrev = true;
       }
     },
-    scroll : function(){
-      if(this.realIndex == 0){
+    scroll: function () {
+      if (this.realIndex == 0) {
         this.allowSlidePrev = false;
       } else {
         this.allowSlidePrev = true;
       }
     },
-    slideChange : function() {
+    slideChange: function () {
       currentSlide = this.activeIndex;
       navColorChange(currentSlide);
-    }
-  }
+    },
+  },
 });
