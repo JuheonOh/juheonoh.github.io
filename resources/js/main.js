4e51efc2ee11fe82600d53516886393b2732bf76
= <dhwngjs01@naver.com>
2023-05-25 02:25:12 +0900

    UPDATE - 메뉴 색상 변경 버그 수정, Swiper 스크롤 이벤트 수정

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,4 @@
 const handleSlideMove = (slideNumber) => {
-  $("ul.navbar-nav li a").removeClass("active");
-
   if ($(".navbar-collapse").hasClass("show")) {
     $(".navbar-toggler").click();
   }
@@ -27,6 +25,7 @@ const swiper = new Swiper(".swiper-container", {
   direction: "vertical",
   mousewheel: true,
   keyboard: true,
+  mouseclick: true,
   slidesPerView: "auto",
   on: {
     touchMove: function () {
@@ -48,4 +47,47 @@ const swiper = new Swiper(".swiper-container", {
       navColorChange(currentSlide);
     },
   },
+});
+
+swiper.on("slideChangeTransitionEnd", function () {
+  var acs = document.querySelectorAll(".swiper-slide-active")[0];
+  var hasVerticalScrollbar = acs.scrollHeight > acs.clientHeight;
+
+  if (hasVerticalScrollbar) {
+    var scrollHeight = acs.scrollHeight;
+    var slideSize = acs.swiperSlideSize;
+    var scrollDifferenceTop = scrollHeight - slideSize;
+
+    acs.addEventListener("wheel", findScrollDirectionOtherBrowsers);
+
+    function findScrollDirectionOtherBrowsers(event) {
+      var scrollTop = acs.scrollTop;
+      scrollHeight = acs.scrollHeight;
+      slideSize = acs.swiperSlideSize;
+      var scrollDifference = scrollHeight - (scrollTop + slideSize);
+      var delta;
+
+      if (event.wheelDelta) {
+        delta = event.wheelDelta;
+      } else {
+        delta = -1 * event.deltaY;
+      }
+
+      if (delta < 0) {
+        if (scrollDifference < 1) {
+          swiper.mousewheel.enable();
+        } else {
+          swiper.mousewheel.disable();
+        }
+      } else if (delta > 0) {
+        if (scrollDifference == scrollDifferenceTop) {
+          swiper.mousewheel.enable();
+        } else {
+          swiper.mousewheel.disable();
+        }
+      }
+    }
+  } else {
+    swiper.mousewheel.enable();
+  }
 });
